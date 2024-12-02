03ecc7371726081b73b55cc7c7f6a6eaaa609e65
AidarIglesias <aidar.iglesias@gmail.com>
2024-12-02 15:48:50 +0100

    new ver

--- src/rubot_control/src/rubot_wall_follower_rg_mpuig.py
+++ src/rubot_control/src/rubot_wall_follower_rg_mpuig.py
@@ -2,26 +2,28 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-import time
 
 pub = None
+current_state = "idle"
+last_state_time = 0
 d = 0
 vx = 0
-wz = 0
-vf = 0
+rate = None
 
 isScanRangesLengthCorrectionFactorCalculated = False
 scanRangesLengthCorrectionFactor = 2
 
 
 def clbk_laser(msg):
-    # En la primera ejecucion, calculamos el factor de correcion
+    # En la primera ejecución, calculamos el factor de corrección
     global isScanRangesLengthCorrectionFactorCalculated
     global scanRangesLengthCorrectionFactor
     
     if not isScanRangesLengthCorrectionFactorCalculated:
-            scanRangesLengthCorrectionFactor = len(msg.ranges) / 360
-            isScanRangesLengthCorrectionFactorCalculated = True
+        scanRangesLengthCorrectionFactor = len(msg.ranges) / 360
+        isScanRangesLengthCorrectionFactorCalculated = True
+
+    # Define los límites de las regiones
     back_min = int(335 * scanRangesLengthCorrectionFactor)
     back_cut_min = int(360 * scanRangesLengthCorrectionFactor - 1)
     back_cut_max = int(0 * scanRangesLengthCorrectionFactor)
@@ -32,7 +34,7 @@ def clbk_laser(msg):
     right_max = int(115 * scanRangesLengthCorrectionFactor)
     fright_min = int(115 * scanRangesLengthCorrectionFactor)
     fright_max = int(155 * scanRangesLengthCorrectionFactor)
-    front_min= int(155 * scanRangesLengthCorrectionFactor)
+    front_min = int(155 * scanRangesLengthCorrectionFactor)
     front_max = int(205 * scanRangesLengthCorrectionFactor)
     fleft_min = int(205 * scanRangesLengthCorrectionFactor)
     fleft_max = int(245 * scanRangesLengthCorrectionFactor)
@@ -41,89 +43,91 @@ def clbk_laser(msg):
     bleft_min = int(295 * scanRangesLengthCorrectionFactor)
     bleft_max = int(335 * scanRangesLengthCorrectionFactor)
 
+    # Define las regiones del láser
     regions = {
-        'back': min(min(msg.ranges[back_min:back_cut_min]),min(msg.ranges[back_cut_max:back_max]), 3),
-        'left':  min(min(msg.ranges[left_min:left_max]), 3),
-        'bleft':  min(min(msg.ranges[bleft_min:bleft_max]), 3),
-        'fleft':  min(min(msg.ranges[fleft_min:fleft_max]), 3),
-        'right':  min(min(msg.ranges[right_min:right_max]), 3),
-        'bright':  min(min(msg.ranges[bright_min:bright_max]), 3),
-        'fright':  min(min(msg.ranges[fright_min:fright_max]), 3),
-        'front':  min(min(msg.ranges[front_min:front_max]), 3),
+        'back': min(min(msg.ranges[back_min:back_cut_min]), min(msg.ranges[back_cut_max:back_max]), 3),
+        'left': min(min(msg.ranges[left_min:left_max]), 3),
+        'bleft': min(min(msg.ranges[bleft_min:bleft_max]), 3),
+        'fleft': min(min(msg.ranges[fleft_min:fleft_max]), 3),
+        'right': min(min(msg.ranges[right_min:right_max]), 3),
+        'bright': min(min(msg.ranges[bright_min:bright_max]), 3),
+        'fright': min(min(msg.ranges[fright_min:fright_max]), 3),
+        'front': min(min(msg.ranges[front_min:front_max]), 3),
     }
 
     take_action(regions)
 
 
 def take_action(regions):
+    global current_state, last_state_time
+    global vx, d
+
+    # Define las transiciones entre estados
+    if rospy.get_time() - last_state_time < 0.5:  # Temporizador para evitar cambios rápidos
+        return
+    last_state_time = rospy.get_time()
+
     msg = Twist()
-    linear_x = 0
-    linear_y = 0
-    angular_z = 0
+    linear_x, linear_y, angular_z = 0, 0, 0
     aux = 1.5
 
-    state_description = ''
+    if current_state == "idle":
+        if regions['front'] > d and regions['left'] > d and regions['right'] > d and regions['back'] > d:
+            current_state = "move_forward"
+        elif regions['front'] < d:
+            current_state = "turn_left"
 
-    if regions['front'] > d and regions['left'] > d and regions['bleft'] > d*aux and regions['fleft'] > d*aux and regions['right'] > d  and regions['bright'] > d*aux  and regions['fright'] > d*aux  and regions['back'] > d:
-        state_description = 'case 1 - nothing'
+    elif current_state == "move_forward":
+        if regions['front'] < d:
+            current_state = "turn_left"
+        elif regions['back'] < d:
+            current_state = "turn_right"
+
+    elif current_state == "turn_left":
+        if regions['left'] < d:
+            current_state = "move_backward"
+        elif regions['fleft'] < d * aux:
+            current_state = "adjust_left"
+
+    elif current_state == "turn_right":
+        if regions['right'] < d:
+            current_state = "move_backward"
+        elif regions['bright'] < d * aux:
+            current_state = "adjust_right"
+
+    elif current_state == "move_backward":
+        if regions['back'] > d:
+            current_state = "idle"
+
+    elif current_state == "adjust_left":
+        if regions['front'] > d:
+            current_state = "idle"
+
+    elif current_state == "adjust_right":
+        if regions['front'] > d:
+            current_state = "idle"
+
+    # Define las acciones para cada estado
+    if current_state == "move_forward":
         linear_x = vx
-        linear_y = 0
-        angular_z = 0
-    elif regions['front'] < d and regions['left'] > d:
-        state_description = 'case 2 - front'
-        linear_x = 0
-        linear_y = vx
-        angular_z = 0
-
-    elif regions['left'] < d and regions['back'] > d:
-        state_description = 'case 3 - left'
+    elif current_state == "move_backward":
         linear_x = -vx
-        linear_y = 0
-        angular_z = 0
-        
+    elif current_state == "turn_left":
+        angular_z = 0.5 * vx
+    elif current_state == "turn_right":
+        angular_z = -0.5 * vx
+    elif current_state == "adjust_left":
+        linear_y = 0.5 * vx
+    elif current_state == "adjust_right":
+        linear_y = -0.5 * vx
 
-    elif regions['back'] < d  and regions['right'] > d:
-        state_description = 'case 4 - back'
-        linear_x = 0
-        linear_y = -vx
-        angular_z = 0
-    
-    elif regions['right'] < d and regions['front'] > d:
-        state_description = 'case 5 - right'
-        linear_x = vx
-        linear_y = 0
-        angular_z = 0
-
-    elif regions['fleft'] < d*aux:
-        state_description = 'case 6 - fleft 2'
-        linear_x = 0
-        linear_y = -vx*0.5
-        angular_z = 0
-
-    elif regions['bleft'] < d*aux:
-        state_description = 'case 7 - bleft 2'
-        linear_x = -vx*0.5
-        linear_y = 0
-        angular_z = 0
-
-    elif regions['bright'] < d*aux:
-        state_description = 'case 8 - brigth 2'
-        linear_x = 0
-        linear_y = -vx*0.5
-        angular_z = 0
-
-    elif regions['fright'] < d*aux:
-        state_description = 'case 9 - rfigth 2'
-        linear_x = vx*0.5
-        linear_y = 0
-        angular_z = 0
-
-    rospy.loginfo(state_description)
+    rospy.loginfo(f"Current State: {current_state}")
     msg.linear.x = linear_x
     msg.linear.y = linear_y
     msg.angular.z = angular_z
     pub.publish(msg)
     rate.sleep()
+
 
 def shutdown():
     msg = Twist()
@@ -131,38 +135,26 @@ def shutdown():
     msg.linear.y = 0
     msg.angular.z = 0
     pub.publish(msg)
-    rospy.loginfo("Stop rUBot")
+    rospy.loginfo("Robot stopped.")
+
 
 def main():
-    global pub
-    global sub
-    global rate
-    global d
-    global vx
-    global wz
-    global vf
+    global pub, rate, d, vx
 
     rospy.init_node('wall_follower')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     sub = rospy.Subscriber('/scan', LaserScan, clbk_laser)
     rospy.on_shutdown(shutdown)
-    rate = rospy.Rate(25)
 
-    d= rospy.get_param("~distance_laser")
-    vx= rospy.get_param("~forward_speed")
-    wz= rospy.get_param("~rotation_speed")
-    vf= rospy.get_param("~speed_factor")
-    
-    
+    rate = rospy.Rate(25)
+    d = rospy.get_param("~distance_laser", 0.5)
+    vx = rospy.get_param("~forward_speed", 0.2)
+
+    rospy.spin()
+
+
 if __name__ == '__main__':
     try:
         main()
-        rospy.spin()
     except rospy.ROSInterruptException:
         shutdown()
-
-    
-
-
-if __name__ == '__main__':
-    main()
